faf21626fa0ace71677a4ff37129320b61b04f77
AnoopReddy <anoop.reddy50@gmail.com>
2024-09-22 21:00:42 -0400

    added debug log and current state

--- Assets/Scripts/StateMachines.cs
+++ Assets/Scripts/StateMachines.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class StateMachines : MonoBehaviour
 {
+    public enum State
+    {
+        idle,
+        walking,
+        swimming,
+        climbing
+    }
+
+    public State currentstate = State.idle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,46 @@ public class StateMachines : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch(currentState)
+        {
+            case State.idle: Idle();  break;
+            case State.walking: Walking(); break;
+            case State.swimming: Swimming(); break;
+            case State.climbing: Climbing(); break;
+            default: break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if(other.name == "Water")
+        {
+            Swimming();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
         
     }
+    void Swimming()
+    {
+        Debug.Log("I am Swimming");
+    }
+    void Climbing()
+    {
+        Debug.Log("I am Climbing");
+    }
+
+    void Idle()
+    {
+        Debug.Log("I am idle");
+    }
+
+    void Walking()
+    {
+        Debug.Log("I am walking");
+    }
 }
+
